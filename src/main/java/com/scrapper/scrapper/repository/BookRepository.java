b6a825e2493ac7429ffec30dc879f84fc36a9d72
EmilGabriel81 <emilgabriel@gmail.com>
2025-08-19 22:42:02 +0300

    returned all books method fix

--- src/main/java/com/scrapper/scrapper/repository/BookRepository.java
+++ src/main/java/com/scrapper/scrapper/repository/BookRepository.java
@@ -8,6 +8,7 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -16,20 +17,20 @@ public class BookRepository {
 
     public List<BookEntity> findAll() {
 
+        List<BookEntity> books = null;
+
         try {
             Document doc = Jsoup.connect(BOOKS_URL).get();
-            Elements books = doc.select(".product_pod");
+            Elements bookElements = doc.select(".product_pod");
+            books = new ArrayList<>();
 
-            for (Element book : books) {
+            for (Element book : bookElements) {
                 String title = book.select("h3 > a").text();
                 String price = book.select(".price_color").text();
                 String pr = price.substring(1);
                 double finalPrice = Double.parseDouble(pr);
 
-                BookEntity bookEntity = BookEntity.builder().withTitle(title).withPrice(finalPrice).build();
-
-          //      BookEntity bookEntity = new BookEntity();
-
+                books.add(BookEntity.builder().withTitle(title).withPrice(finalPrice).build());
                 if (finalPrice < 25) {
                     System.out.println(title + " - " + price);
                 }
@@ -38,6 +39,6 @@ public class BookRepository {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return books;
     }
 }
